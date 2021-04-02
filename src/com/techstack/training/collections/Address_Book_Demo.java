21ba6ea201837c2ff2faaaeed063d0935bee0b5e
gsgautam2492 <81697109+gsgautam2492@users.noreply.github.com>
2021-04-02 20:45:14 +0530

    Changes to Take Address Book Input from Console for UC2.

--- src/com/techstack/training/collections/Address_Book_Demo.java
+++ src/com/techstack/training/collections/Address_Book_Demo.java
@@ -36,6 +36,10 @@ class Address_Book {
 		this.email = email;
 	}
 
+	public Address_Book() {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public String toString() {
 		return "Address_Book [fname=" + fname + ", lname=" + lname + ", address_Line1=" + address_Line1 + ", city="
@@ -43,31 +47,126 @@ class Address_Book {
 				+ "]";
 	}
 
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getAddress_Line1() {
+		return address_Line1;
+	}
+
+	public void setAddress_Line1(String address_Line1) {
+		this.address_Line1 = address_Line1;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip_Code() {
+		return zip_Code;
+	}
+
+	public void setZip_Code(String zip_Code) {
+		this.zip_Code = zip_Code;
+	}
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
 
 public class Address_Book_Demo {
 	public static void main(String[] args) {
 		LinkedHashSet<Address_Book> hs = new LinkedHashSet<Address_Book>();
 		// Creating Books
-		Address_Book b1 = new Address_Book("Gautam", "Sharma", "Apt 123", "New Delhi", "Delhi", "110045", "9911234777",
-				"xyz@gmail.com");
-		Address_Book b2 = new Address_Book("Garvit", "Mangal", "Apt 456", "New Delhi", "Delhi", "110045", "9911350877",
-				"abc@gmail.com");
-		Address_Book b3 = new Address_Book("Gautam", "Sharma", "Apt 123", "New Delhi", "Delhi", "110045", "9911350878",
-				"ab123@gmail.com");
+		/*
+		 * Address_Book b1 = new Address_Book("Gautam", "Sharma", "Apt 123",
+		 * "New Delhi", "Delhi", "110045", "9911234777", "xyz@gmail.com"); Address_Book
+		 * b2 = new Address_Book("Garvit", "Mangal", "Apt 456", "New Delhi", "Delhi",
+		 * "110045", "9911350877", "abc@gmail.com"); Address_Book b3 = new
+		 * Address_Book("Gautam", "Sharma", "Apt 123", "New Delhi", "Delhi", "110045",
+		 * "9911350878", "ab123@gmail.com");
+		 */
 		// Adding to Address_Book objects to Linked hash set
-		hs.add(b1);
-		hs.add(b2);
-		hs.add(b3);
-		hs.add(b1);
-		// Traversing Linked hash set
 		System.out.println("Welcome to Address Book Program \n \n");
+		Scanner keyboard = new Scanner(System.in);
+		String loopCheck = "";
+		while (!loopCheck.equals("N")) {
+			Address_Book b1 = new Address_Book();
+			System.out.print("Please enter fname: ");
+			String fname = keyboard.nextLine(); // local variable
+			b1.setFname(fname);
+			System.out.print("\nPlease enter lname: ");
+			String lname = keyboard.nextLine();
+			b1.setLname(lname);
+			System.out.print("\nPlease enter addressLine1: ");
+			String addressLine1 = keyboard.nextLine();
+			b1.setAddress_Line1(addressLine1);
+			System.out.print("\nPlease enter city: ");
+			String city = keyboard.nextLine();
+			b1.setCity(city);
+			System.out.print("\nPlease enter state: ");
+			String state = keyboard.nextLine();
+			b1.setState(state);
+			System.out.print("\nPlease enter zip: ");
+			String zip = keyboard.nextLine();
+			b1.setZip_Code(zip);
+			System.out.print("\nPlease enter phone: ");
+			String phone = keyboard.nextLine();
+			b1.setPhone_no(phone);
+			System.out.print("\nPlease enter email: ");
+			String email = keyboard.nextLine();
+			b1.setEmail(email);
+			hs.add(b1);
+			System.out.print("\nDo you want to continue:(Y/N) ");
+			loopCheck = keyboard.nextLine();
+		}
+		// Traversing Linked hash set
 		for (Address_Book b : hs) {
-			//System.out.println(b.fname + " " + b.lname + " " + b.address_Line1 + " " + b.city + " " + b.state + " "
-				//	+ b.zip_Code + " " + b.phone_no + " " + b.email);
+			// System.out.println(b.fname + " " + b.lname + " " + b.address_Line1 + " " +
+			// b.city + " " + b.state + " "
+			// + b.zip_Code + " " + b.phone_no + " " + b.email);
 			System.out.println(b.toString());
 		}
-		//Printing whole Linked hash set
-		System.out.println("\n \n"+hs.toString());
+		// Printing whole Linked hash set
+		System.out.println("\n \n" + hs.toString());
 	}
+
 }
